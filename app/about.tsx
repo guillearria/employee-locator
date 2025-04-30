f592a10dbd502877c75fab1b31aa190af5932946
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2025-04-30 16:04:58 -0400

    create about component

---
+++ app/about.tsx
@@ -0,0 +1,18 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <Text>About Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
