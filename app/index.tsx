38b40dc41cddf9d38a65ee3bad68d95277e6d429
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2025-04-30 16:18:55 -0400

    add basic link

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -6,6 +7,7 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/about" style={styles.link}>About</Link>
     </View>
   );
 }
