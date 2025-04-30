b9500d905c869d2b4b012ddbebae4122724f4750
Guillermo Arria <guillermo.arriadevoe@gmail.com>
2025-04-30 16:07:59 -0400

    add stack screens

--- app/about.tsx
+++ app/about.tsx
@@ -3,7 +3,7 @@ import { Text, View, StyleSheet } from "react-native";
 export default function About() {
   return (
     <View style={styles.container}>
-      <Text>About Screen</Text>
+      <Text style={styles.text}>About Screen</Text>
     </View>
   );
 }
@@ -14,5 +14,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
   },
 });
